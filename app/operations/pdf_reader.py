aebc35297d9dbea37e8098f127844ad05edc8710
Dreaminds.ai <dreaminds.ai@gmail.com>
2023-11-17 09:33:33 +0530

    updated text extract

--- app/operations/pdf_reader.py
+++ app/operations/pdf_reader.py
@@ -1,34 +1,23 @@
 # Extract data from pdf
 
+from io import BytesIO
 from PyPDF2 import PdfReader
-import os
 
-def get_pdf_text(pdf_file_path: str) -> str:
+def get_pdf_text(pdf_content: bytes) -> str:
     """
-    Extract text from a PDF file and return it as a string.
+    Extract text from a PDF file's content.
 
     Args:
-        pdf_file_path (str): The path to the PDF file.
+        pdf_content (bytes): The binary content of the PDF file.
 
     Returns:
         str: The extracted text from the PDF.
-
-     Raises:
-        FileNotFoundError: If the PDF file does not exist.
-        ValueError: If the file does not have a '.pdf' extension.
     """
-    # Check if the file exists
-    if not os.path.exists(pdf_file_path):
-        raise FileNotFoundError(f"PDF file '{pdf_file_path}' not found.")
-    
-    # Check if the file has a '.pdf' extension
-    if not pdf_file_path.lower().endswith('.pdf'):
-        raise ValueError(f"Invalid file extension. The file must have a '.pdf' extension.")
-    
-    text = ""
-    pdf_reader = PdfReader(pdf_file_path)
-    
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    
-    return text
+    try:
+        pdf_reader = PdfReader(BytesIO(pdf_content))
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        return text
+    except Exception as e:
+        return str(e)
