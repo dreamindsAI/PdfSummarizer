fb2f1a43da66a7683a558a480ac8e37a387a1420
Dreaminds.ai <dreaminds.ai@gmail.com>
2023-11-17 09:35:18 +0530

    text extract enpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from io import BytesIO
+from operations.pdf_reader import get_pdf_text
 import uvicorn
 
 # Create an instance of the FastAPI class
@@ -8,6 +10,19 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Server is running.."}
+
+@app.post("/pdf/")
+async def upload_pdf(file: UploadFile) -> dict:
+
+    if not file.filename.lower().endswith(".pdf"):
+        return {"error": "Please upload a PDF file."}
+
+    pdf_content = await file.read()
+    
+    # Process the PDF using get_pdf_text function
+    text = get_pdf_text(pdf_content)
+
+    return {"text": text}
 
 if __name__ == "__main__":
 
